ab055f710e30d7b702d439fb99ad9aa1a28e9c14
S B <evren_nari@hotmail.com>
2020-02-14 16:47:56 +0300

    Run forest run

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,36 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] Transform target;
-    NavMeshAgent navMeshAgent;
-    Camera mainCamera;
-    Ray lastRay;
+    private NavMeshAgent navMeshAgent;
+    private Camera mainCamera;
+    private Ray ray;
+    private Animator animator;
+    private Vector3 worldVelocity;
+    private Vector3 localVelocity;
+    private float playerSpeed;
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         mainCamera = Camera.main;
-         
+        animator = GetComponent<Animator>();
     }
 
-    
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             MoveToCursor();
         }
-        //Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
-        //navMeshAgent.SetDestination(target.position);
+        AnimationUpdate();
     }
+
+    private void AnimationUpdate()
+    {
+        worldVelocity = navMeshAgent.velocity;
+        localVelocity = transform.InverseTransformDirection(worldVelocity);
+        playerSpeed = localVelocity.z;
+        animator.SetFloat("Forward", playerSpeed);
+    }
+
     private void MoveToCursor()
     {
         RaycastHit hit;
-        lastRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(lastRay, out hit))
+        ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+   
+        if (Physics.Raycast(ray, out hit))
         {
             navMeshAgent.SetDestination(hit.point);
         }
---
+++ Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    [SerializeField] Transform camTarget;
+    //Order of execution 
+    void LateUpdate()
+    {
+        transform.position = camTarget.position;
+    }
+}
